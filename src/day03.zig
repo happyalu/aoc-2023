1058ec727ff19f13d114f4ae403c7a0d10f343b5
Alok Parlikar <alok@parlikar.com>
2023-12-03 14:17:59 +0530

    attempted day 03 part 2

--- src/day03.zig
+++ src/day03.zig
@@ -14,15 +14,24 @@ const data = @embedFile("data/day03.txt");
 pub fn main() !void {
     // look at 3 lines at a time to check for symbols.
     var lineBuf: [3][]const u8 = undefined;
+    var gearBuf: [3][1024]Gear = undefined;
 
     var part_sum: u32 = 0;
     var gear_ratio: u32 = 0;
-    _ = gear_ratio;
 
     var iter = tokenizeSeq(u8, data, "\n");
 
     lineBuf[1] = "." ** 1024;
     lineBuf[2] = iter.next().?;
+
+    for (&gearBuf) |*gb| {
+        for (gb) |*x| {
+            x.* = Gear{
+                .part_count = 0,
+                .ratio = 1,
+            };
+        }
+    }
 
     while (iter.next()) |line| {
         // read next input
@@ -30,17 +39,44 @@ pub fn main() !void {
         lineBuf[1] = lineBuf[2];
         lineBuf[2] = line;
 
-        part_sum += try processParts(lineBuf);
+        try processParts(lineBuf, &gearBuf, &part_sum, &gear_ratio);
+
+        for (gearBuf[0]) |g| {
+            if (g.part_count == 2) {
+                gear_ratio += g.ratio;
+            }
+        }
+
+        std.mem.copy(Gear, &gearBuf[0], &gearBuf[1]);
+        std.mem.copy(Gear, &gearBuf[1], &gearBuf[2]);
+        for (&gearBuf[2]) |*gb| {
+            gb.* = Gear{
+                .part_count = 0,
+                .ratio = 1,
+            };
+        }
     }
 
     lineBuf[0] = lineBuf[1];
     lineBuf[1] = lineBuf[2];
     lineBuf[2] = "." ** 1024;
 
-    part_sum += try processParts(lineBuf);
+    try processParts(lineBuf, &gearBuf, &part_sum, &gear_ratio);
+
+    for (gearBuf[0]) |g| {
+        if (g.part_count == 2) {
+            gear_ratio += g.ratio;
+        }
+    }
 
     print("part_sum: {d}\n", .{part_sum});
+    print("gear_ratio: {d}\n", .{gear_ratio});
 }
+
+const Gear = struct {
+    part_count: u32,
+    ratio: u32,
+};
 
 fn isSymbol(c: u8) bool {
     if (c != '.' and !std.ascii.isDigit(c)) {
@@ -49,8 +85,8 @@ fn isSymbol(c: u8) bool {
     return false;
 }
 
-fn processParts(lineBuf: [3][]const u8) !u32 {
-    var part_sum: u32 = 0;
+fn processParts(lineBuf: [3][]const u8, gearBuf: *[3][1024]Gear, part_sum: *u32, gear_ratio: *u32) !void {
+    _ = gear_ratio;
     var b_pos: ?usize = null;
     var e_pos: usize = 0;
 
@@ -62,32 +98,30 @@ fn processParts(lineBuf: [3][]const u8) !u32 {
             continue;
         }
 
-        var is_part: bool = false;
-
         if (b_pos != null) {
             // a full number has been matched; let's see if it is a part number.
 
-            blk: for (lineBuf) |l| {
+            blk: for (lineBuf, 0..) |l, idx1| {
                 var i = if (b_pos.? > 0) b_pos.? - 1 else 0;
                 var j = if (e_pos < l.len) e_pos + 1 else l.len;
-                print("{d} to {d}\n", .{ i, j });
-                for (l[i..j]) |c| {
+                for (l[i..j], 0..) |c, idx2| {
                     if (isSymbol(c)) {
-                        is_part = true;
+                        var num = try parseInt(u32, cl[b_pos.?..e_pos], 10);
+                        part_sum.* += num;
+
+                        if (c == '*') {
+                            gearBuf[idx1][idx2 + i].part_count += 1;
+                            gearBuf[idx1][idx2 + i].ratio *= num;
+                        }
+
                         break :blk;
                     }
                 }
             }
 
-            if (is_part) {
-                print("{s}\n", .{cl[b_pos.?..e_pos]});
-                part_sum += try parseInt(u32, cl[b_pos.?..e_pos], 10);
-            }
-
             b_pos = null;
         }
     }
-    return part_sum;
 }
 
 // Useful stdlib functions
