cb9657412ad527c71cacdc6b2f9574bcb3ed37e4
Alok Parlikar <alok@parlikar.com>
2023-12-06 11:13:57 +0530

    attempted day 06 part 1

--- src/day06.zig
+++ src/day06.zig
@@ -8,9 +8,43 @@ const BitSet = std.DynamicBitSet;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("data/day06.txt");
+const data = @embedFile("data/day06.example.txt");
+//const data = @embedFile("data/day06.txt");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var timer = try std.time.Timer.start();
+
+    var iter = tokenizeAny(u8, data, ":\n");
+    _ = iter.next(); // Time:
+    var iter_time = tokenizeAny(u8, iter.next().?, " ");
+    _ = iter.next(); // Distance;
+    var iter_dist = tokenizeAny(u8, iter.next().?, " ");
+
+    // part1
+    var part1: u32 = 1;
+    while (true) {
+        var t_str = iter_time.next() orelse break;
+        var d_str = iter_dist.next() orelse unreachable;
+
+        var t = try parseInt(u32, t_str, 10);
+        var d = try parseInt(u32, d_str, 10);
+
+        var matches: u32 = 0;
+
+        var i: u32 = 1;
+        while (i < t) : (i += 1) {
+            if (i * (t - i) > d) {
+                matches += 1;
+            }
+        }
+
+        part1 *= matches;
+    }
+
+    const part2 = 0;
+
+    std.debug.print("part1 {} part2 {}\nmain() total time {}\n", .{ part1, part2, std.fmt.fmtDuration(timer.read()) });
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
