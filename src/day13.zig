23b6df779b903d54ad7bbcd29e4d44484f90ec71
Alok Parlikar <alok@parlikar.com>
2023-12-13 23:39:25 +0530

    optimized day13

--- src/day13.zig
+++ src/day13.zig
@@ -5,18 +5,6 @@ const data = @embedFile("data/day13.txt");
 
 pub fn main() !void {
     var timer = try std.time.Timer.start();
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    var alloc = arena.allocator();
-
-    var h = std.ArrayList(usize).init(alloc);
-    defer h.deinit();
-
-    var v = std.ArrayList(usize).init(alloc);
-    defer v.deinit();
-
-    var linelist = std.ArrayList([]const u8).init(alloc);
-    defer linelist.deinit();
 
     var iter = std.mem.tokenizeSequence(u8, data, "\n\n");
 
@@ -24,69 +12,75 @@ pub fn main() !void {
     var part2: usize = 0;
 
     while (iter.next()) |pattern| {
-        h.clearRetainingCapacity();
-        v.clearRetainingCapacity();
-        linelist.clearRetainingCapacity();
-
-        var lines = std.mem.tokenize(u8, pattern, "\n");
-        var h_len = lines.peek().?.len;
-
-        try h.appendNTimes(0, h_len);
-
-        var v_idx: usize = 0;
-        while (lines.next()) |line| : (v_idx += 1) {
-            try linelist.append(line);
-            try v.append(0);
-
-            //print("{s}\n", .{line});
-            for (1..h_len) |i| {
-                var ref_len = @min(i, h_len - i);
-                for (0..ref_len) |j| {
-                    //print("{} {} {c} {c}\n", .{ i, j, line[i - j - 1], line[i + j] });
-                    if (line[i - j - 1] != line[i + j]) {
-                        h.items[i] += 1;
-                    }
-                }
-            }
-        }
-
-        for (1..linelist.items.len) |i| {
-            var ref_len = @min(i, linelist.items.len - i);
-            for (0..ref_len) |j| {
-                var a = linelist.items[i - j - 1];
-                var b = linelist.items[i + j];
-                for (a, b) |c1, c2| {
-                    if (c1 != c2) {
-                        v.items[i] += 1;
-                    }
-                }
-            }
-        }
-
-        for (h.items[1..h.items.len], 1..) |x, idx| {
-            switch (x) {
-                0 => part1 += idx,
-                1 => part2 += idx,
-                else => continue,
-            }
-        }
-
-        for (v.items[1..v.items.len], 1..) |x, idx| {
-            switch (x) {
-                0 => part1 += 100 * idx,
-                1 => part2 += 100 * idx,
-                else => continue,
-            }
-        }
-
-        //print("v: {any}\n", .{v.items});
-        //print("h: {any}\n", .{h.items});
+        if (pattern.len == 0) continue;
+        var width = std.mem.indexOfScalar(u8, pattern, '\n').?;
+        part1 += @reduce(.Add, process(width, pattern, 0) * usize2{ 1, 100 });
+        part2 += @reduce(.Add, process(width, pattern, 1) * usize2{ 1, 100 });
     }
 
     print("day 13: part1 = {}\n", .{part1});
     print("day 13: part2 = {}\n", .{part2});
     print("day 13: main() total: {}\n", .{std.fmt.fmtDuration(timer.read())});
 }
+
+fn process(w: usize, pattern: []const u8, num_smudges: usize) usize2 {
+    var out = usize2{ 0, 0 };
+
+    // look for vertical mirrors
+    x: for (1..w) |i| {
+        var ref_len = @min(i, w - i);
+        var diff: usize = 0;
+        var y: usize = 0;
+        while (y <= pattern.len - w) : (y += w + 1) {
+            var line = pattern[y .. y + w];
+
+            for (0..ref_len) |j| {
+                //print("{} {} {c} {c}\n", .{ i, j, line[i - j - 1], line[i + j] });
+                if (line[i - j - 1] != line[i + j]) {
+                    diff += 1;
+                    if (diff > num_smudges) {
+                        continue :x;
+                    }
+                }
+            }
+        }
+
+        if (diff == num_smudges) {
+            out[0] = i;
+            break;
+        }
+    }
+
+    var num_lines = (pattern.len + 1) / (w + 1);
+    y: for (1..num_lines) |i| {
+        var diff: usize = 0;
+        var ref_len = @min(i, num_lines - i);
+        for (0..ref_len) |j| {
+            var a_idx = (i - j - 1) * (w + 1);
+            var b_idx = (i + j) * (w + 1);
+
+            var a = pattern[a_idx .. a_idx + w];
+            var b = pattern[b_idx .. b_idx + w];
+
+            //print("{}\n{s}\n{s}\n\n", .{ i, a, b });
+            for (a, b) |c1, c2| {
+                if (c1 != c2) {
+                    diff += 1;
+                    if (diff > num_smudges) continue :y;
+                }
+            }
+        }
+
+        if (diff == num_smudges) {
+            out[1] = i;
+            break;
+        }
+    }
+
+    return out;
+}
+
+const usize2 = @Vector(2, usize);
 
 const print = std.debug.print;
 
