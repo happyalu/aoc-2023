9ff06f00eb1725d7bd8191a45988ac17216bde89
Alok Parlikar <alok@parlikar.com>
2023-12-15 16:44:13 +0530

    implemented day15

--- src/day15.zig
+++ src/day15.zig
@@ -1,43 +1,107 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
 
 const data = @embedFile("data/day15.txt");
+//const data = @embedFile("data/day15.example.txt");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var timer = try std.time.Timer.start();
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var alloc = arena.allocator();
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+    var iter = std.mem.tokenize(u8, data, ",\n");
+
+    var part1: usize = 0;
+
+    var hm = HashMap.init(alloc);
+    while (iter.next()) |x| {
+        if (x.len == 0) continue;
+        var res = hash(x);
+        part1 += res;
+
+        try hm.runStep(x);
+    }
+
+    print("day15 part1: {}\n", .{part1});
+    print("day15 part2: {}\n", .{hm.power()});
+    print("day15 all main(): {}\n", .{std.fmt.fmtDuration(timer.read())});
+}
+
+fn hash(s: []const u8) u8 {
+    var out: u16 = 0;
+    for (s) |c| {
+        out += c;
+        out *= 17;
+        out %= 256;
+    }
+    return @intCast(out);
+}
+
+const Lens = struct {
+    label: []const u8,
+    power: u8,
+};
+
+const HashMap = struct {
+    slots: [256]std.ArrayList(Lens),
+
+    fn init(alloc: std.mem.Allocator) HashMap {
+        var out: HashMap = undefined;
+        for (&out.slots) |*s| {
+            s.* = std.ArrayList(Lens).init(alloc);
+        }
+
+        return out;
+    }
+
+    fn runStep(self: *HashMap, step: []const u8) !void {
+        var opIdx = std.mem.indexOfAny(u8, step, "=-") orelse unreachable;
+        var label = step[0..opIdx];
+        var slot = hash(label);
+        var op = step[opIdx];
+        switch (op) {
+            '=' => {
+                var p_str = step[opIdx + 1 ..];
+                var p = try std.fmt.parseInt(u8, p_str, 10);
+                for (self.slots[slot].items) |*l| {
+                    if (std.mem.eql(u8, l.label, label)) {
+                        l.power = p;
+                        break;
+                    }
+                } else {
+                    try self.slots[slot].append(.{ .label = label, .power = p });
+                }
+            },
+            '-' => {
+                var list = self.slots[slot].items;
+                for (list, 0..) |l, i| {
+                    if (std.mem.eql(u8, l.label, label)) {
+                        _ = self.slots[slot].orderedRemove(i);
+                        break;
+                    }
+                }
+            },
+            else => unreachable,
+        }
+    }
+
+    fn power(self: HashMap) usize {
+        var out: usize = 0;
+        for (self.slots, 1..) |s, slotIdx| {
+            for (s.items, 1..) |l, lensIdx| {
+                out += slotIdx * lensIdx * l.power;
+            }
+        }
+        return out;
+    }
+};
+
+test "hash(HASH)" {
+    try std.testing.expect(hash("HASH") == 52);
+}
 
 const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
 
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
