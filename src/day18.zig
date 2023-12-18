a9ba072d2b84fbff2a6718db45d66219fd69351a
Alok Parlikar <alok@parlikar.com>
2023-12-18 15:10:15 +0530

    day18: implemented parts 1 and 2 properly

--- src/day18.zig
+++ src/day18.zig
@@ -1,148 +1,102 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
 
 const data = @embedFile("data/day18.txt");
 //const data = @embedFile("data/day18.example.txt");
 
 pub fn main() !void {
     var timer = try std.time.Timer.start();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var alloc = arena.allocator();
 
-    const w = 1000;
-    const h = 1000;
-    var ground: [h][w]Node = undefined;
+    var corners = std.ArrayList(Node).init(alloc);
 
-    for (0..h) |y| {
-        for (0..w) |x| {
-            ground[y][x] = .{ .hole = false, .label = '.' };
-        }
-    }
+    try read_input(&corners, Part.one);
+    print("day 18 part1: {}\n", .{try solve(corners)});
 
-    ground[300][300].hole = true;
-    ground[300][300].label = '.';
+    try read_input(&corners, Part.two);
+    print("day 18 part2: {}\n", .{try solve(corners)});
 
-    var x: usize = 300;
-    var y: usize = 300;
-
-    var prev_dir: u8 = '0';
-    var iter = std.mem.tokenize(u8, data, "\n");
-    var prev_line_end: ?*Node = null;
-    while (iter.next()) |line| {
-        var part_iter = std.mem.tokenize(u8, line, " ");
-        var dir = part_iter.next().?;
-        var count = try std.fmt.parseInt(usize, part_iter.next().?, 10);
-        var color = part_iter.next().?;
-        _ = color;
-
-        var p: *usize = undefined;
-        var step: isize = 0;
-        switch (dir[0]) {
-            'R' => {
-                p = &x;
-                step = 1;
-            },
-            'L' => {
-                p = &x;
-                step = -1;
-            },
-            'D' => {
-                p = &y;
-                step = 1;
-            },
-            'U' => {
-                p = &y;
-                step = -1;
-            },
-            else => unreachable,
-        }
-
-        var end: *Node = undefined;
-        for (0..count) |i| {
-            print("{c}\n", .{dir[0]});
-            if (step == 1) p.* += 1 else p.* -= 1;
-            ground[y][x].hole = true;
-            //ground[y][x].label = '.';
-
-            if (i == 0 and prev_line_end != null) {
-                if (prev_dir == 'D' and dir[0] == 'R') prev_line_end.?.label = 'L' else if (prev_dir == 'L' and dir[0] == 'U') prev_line_end.?.label = 'L' else if (prev_dir == 'D' and dir[0] == 'L') prev_line_end.?.label = 'J' else if (prev_dir == 'R' and dir[0] == 'U') prev_line_end.?.label = 'J' else prev_line_end.?.label = '.';
-            }
-            if (dir[0] == 'D' or dir[0] == 'U') ground[y][x].label = '|';
-
-            end = &ground[y][x];
-        }
-        prev_line_end = end;
-        prev_dir = dir[0];
-    }
-
-    ground[300][300].label = '.';
-    var part1: usize = 0;
-    for (0..h) |i| {
-        var inside: bool = false;
-
-        for (0..w) |j| {
-            if (ground[i][j].label != '.') {
-                inside = !inside;
-            }
-            if (ground[i][j].hole) {
-                part1 += 1;
-            } else {
-                if (inside) {
-                    print("{} {}\n", .{ i, j });
-                    part1 += 1;
-                }
-            }
-        }
-    }
-
-    for (0..h) |i| {
-        for (0..w) |j| {
-            print("{c}", .{ground[i][j].label});
-        }
-        print("\n", .{});
-    }
-
-    //print("{any}\n", .{ground});
-    print("day 18 part1: {}\n", .{part1});
     print("day 18 all main(): {}\n", .{std.fmt.fmtDuration(timer.read())});
 }
 
+fn read_input(corners: *std.ArrayList(Node), part: Part) !void {
+    corners.clearRetainingCapacity();
+
+    var prev_corner = Node{
+        .x = 0,
+        .y = 0,
+    };
+    try corners.append(prev_corner);
+
+    var iter = std.mem.tokenize(u8, data, "\n");
+    while (iter.next()) |line| {
+        var part_iter = std.mem.tokenize(u8, line, " ");
+        var dir_str = part_iter.next().?;
+        var count_str = part_iter.next().?;
+        var color = part_iter.next().?;
+
+        var count: isize = undefined;
+        var dir: u8 = undefined;
+
+        switch (part) {
+            .one => {
+                count = try std.fmt.parseInt(isize, count_str, 10);
+                dir = dir_str[0];
+            },
+            .two => {
+                count = try std.fmt.parseInt(isize, color[2..7], 16);
+                dir = switch (color[7]) {
+                    '0' => 'R',
+                    '1' => 'D',
+                    '2' => 'L',
+                    '3' => 'U',
+                    else => unreachable,
+                };
+            },
+        }
+
+        var c = prev_corner;
+        switch (dir) {
+            'R' => c.x += count,
+            'L' => c.x -= count,
+            'U' => c.y -= count,
+            'D' => c.y += count,
+            else => unreachable,
+        }
+
+        try corners.append(c);
+        prev_corner = c;
+    }
+}
+
+fn solve(corners: std.ArrayList(Node)) !isize {
+    var n = corners.items.len;
+    var area: isize = 0;
+    var perimeter: isize = 0;
+
+    // calculate area using shoelace formula
+    for (0..n) |i| {
+        var p1 = corners.items[i];
+        var p2 = corners.items[(i + 1) % n];
+        area += (p1.y + p2.y) * (p1.x - p2.x);
+        perimeter += try std.math.absInt(p1.y - p2.y) + try std.math.absInt(p1.x - p2.x);
+    }
+
+    area = @divExact(area, 2);
+
+    // use Pick's theorem to calculate all the points
+    return area + 1 + @divExact(perimeter, 2);
+}
+
+const Part = enum { one, two };
+
 const Node = struct {
-    hole: bool,
-    label: u8,
+    x: isize,
+    y: isize,
 };
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
 const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
 
 // Generated from template/template.zig.
 // Run `zig build generate` to update.
