4c1d06ad4d208c3ad1c518217c3ee34aa927cac3
Alok Parlikar <alok@parlikar.com>
2023-12-19 16:25:32 +0530

    day19: cleanup

--- src/day19.zig
+++ src/day19.zig
@@ -1,12 +1,4 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
 
 //const data = @embedFile("data/day19.txt");
 const data = @embedFile("data/day19.example.txt");
